ad199ba821cf738bbecf96072f3cb7d2b0ecbfaa
TrudyCyns <gcyn51a@gmail.com>
2022-06-22 14:55:59 +0300

    Finished Video Manipulation

--- Custom-Video-Player/scripts.js
+++ Custom-Video-Player/scripts.js
@@ -22,18 +22,44 @@ function updateButton() {
 }
 
 function skip() {
-    video.currentTime += parseFloat(this.dataset.skip)
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
-    video[this.name]=this.value;
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  // Convert to percent
+  const percent = (video.currentTime / video.duration) * 100;
+  // Update flex-basis property of progress bar in CSS.
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+// To update progress bar on click on seek to clicked position
+function scrub(e) {
+  // Divide clicked px by actual width (percentage) multiply by duration
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 
 // Hook up event listeners.
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+// To update progress bar
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip))
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+skipButtons.forEach((button) => button.addEventListener('click', skip));
+ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener('mousemove', handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+// If mousedown is true, scrub function run.
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
